f02fcb55bd97528b7ead946a41b54203834e0f16
ADMIN <ehddbsdl22@naver.com>
2021-08-09 11:12:01 +0900

    20210809

--- src/main/java/com/anyfive/test/springboot/domain/posts/Posts.java
+++ src/main/java/com/anyfive/test/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.anyfive.test.springboot.domain.posts;
 
+import com.anyfive.test.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,6 +31,11 @@ public class Posts {
         this.content = content;
         this.author = author;
 
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
     }
 
 }
